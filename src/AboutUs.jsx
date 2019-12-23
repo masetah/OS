097e2a3662cb262c6a538fe03ed7919079d9c53f
Maysam Taherian <masetaherian@gmail.com>
2019-12-23 12:16:25 -0600

    changed image of lawnmower

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -12,14 +12,15 @@ function AboutUs() {
         </Cell>   
         <Cell col={6}>
            <div className="about-us">
+                <img src="./rider-mower.png" alt="lawn mower" height="50%" width="50%" style={{paddingLeft:'20px'}}/>
                 <h1>About Us</h1>
-                <h5 style={{marginTop:'20px'}}>Our 25 years of experience means you will get hassle free service with great results. We specialize in beautifying and creating curb appeal for any home or 
+                <h5 style={{marginTop:'10px'}}>Our 25 years of experience means you will get hassle free service with great results. We specialize in beautifying and creating curb appeal for any home or 
                 business. When you choose O&S your guests will get that wow factor when walking up!
                 </h5>
                 <hr/>
                 <h3>Services</h3>
                 <h5>Mow Service | Tree and Shrub Trimming | Flowerbed Installation</h5>
-                <img src="./lawn-mower.png" alt="lawn mower" height="60%" width="60%"/>
+                
                 
             </div>
         </Cell>
