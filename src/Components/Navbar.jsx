ebf95694620672883085c7c4758f72747cd45704
Maysam Taherian <masetaherian@gmail.com>
2020-01-06 11:54:10 -0600

    contactPage update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,13 +10,16 @@ class Navbar extends Component {
             <img className='logo' src='OS-logo.png' alt='OS logo' />
             <div className='info-div'>
                 <h4> Offering Commercial and Residential Services to Ellis County and Dallas Since 2004.</h4>
-                <hr/>
-                <h5 style={{paddingTop:"20px"}}> Mow Service | Tree Service | Irrigation Repair | Drainage | Sod Install | Free Estimate</h5>
+                {/* <hr/> */}
                 <div className="social-links">
+                    <hr style={{width:'25%'}}/>
                     <a href="tel: 972-268-2450" style={{color:'white', margin:'0'}}><i className="fas fa-phone-square"></i></a>
                     <a href="https://www.facebook.com/OS-Landscaping-123095288286956" style={{color:'white', margin:'0'}}><i className="fab fa-facebook-square"/></a>
                     <a href="mailto:os_landscaping@yahoo.com" style={{color:'white', margin:'0'}}><i className="fas fa-envelope-square"/></a>
+                    <hr style={{width:'25%'}}/>
                 </div>
+                <h5 style={{paddingTop:"10px"}}> Mow Service | Tree Service | Irrigation Repair | Drainage | Sod Install | Free Estimate</h5>
+
                 </div>
 
             </div>
--- src/Views/ContactPage.jsx
+++ src/Views/ContactPage.jsx
@@ -14,11 +14,18 @@ function ContactPage() {
             <Cell col={6}>
                 <img src="OS-logo.png" alt="OS-Landscaping" height='150px' width='160px' style={{borderRadius:'25%'}}/>
                 <p>Est. 2004</p>
+                
+                <div className='info-div'>
                 <h1>Contact Us</h1>
                 <div className="social-links">
+                    <hr style={{width:'25%'}}/>
                     <a href="tel: 972-268-2450" style={{color:'white', margin:'0'}}><i className="fas fa-phone-square"></i></a>
                     <a href="https://www.facebook.com/OS-Landscaping-123095288286956" style={{color:'white', margin:'0'}}><i className="fab fa-facebook-square"/></a>
                     <a href="mailto:os_landscaping@yahoo.com" style={{color:'white', margin:'0'}}><i className="fas fa-envelope-square"/></a>
+                    <hr style={{width:'25%'}}/>
+                </div>
+                <h5 style={{paddingTop:"10px"}}> Mow Service | Tree Service | Irrigation Repair | Drainage | Sod Install | Free Estimate</h5>
+
                 </div>
 
             </Cell>
